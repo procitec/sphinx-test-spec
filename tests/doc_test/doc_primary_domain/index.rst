d919b5b3bca79180ddf002bdb27434264d2c733b
Joerg Kreuzberger <j.kreuzberger@procitec.de>
2023-02-10 15:27:00 +0100

    test primary domain usage

---
+++ tests/doc_test/doc_primary_domain/index.rst
@@ -0,0 +1,12 @@
+Testing your calculator app
+===========================
+
+.. case:: Summarize and Clear values in calculator
+
+    .. action:: Type '2+2' into your systems calculator app and press '<enter>'.
+
+        .. reaction:: The calculator sums and shows you '4' as result.
+
+    .. action:: Clear the results, e.g. by typing '<C>' or another button for clearing the results in your app
+
+        .. reaction:: The calculator does show '0' or shows nothing
---
+++ tests/test_doc_primary_domain.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
+import sys
+
+@pytest.mark.parametrize("test_app", [{"buildername": "html", "srcdir": "doc_test/doc_primary_domain", "warnings": sys.stderr}], indirect=True)
+def test_doc_primary_domain(test_app):
+    app = test_app
+    app.build()
+    html = Path(app.outdir, "index.html").read_text()
+
+    assert '<h2>Summarize and Clear values in calculator' in html
+
+    assert '<tr class="test-action-row row-even"><td class="test-action-id">1.0</td>' in html
+    assert '<td class="test-action"><p>Type ‘2+2’ into your systems calculator app and press ‘&lt;enter&gt;’.' in html
+    assert 'The calculator sums and shows you ‘4’ as result.</p></td>' in html
+    assert html.count('<tr class=') == 3
+    assert html.count('<table class=') == 1
